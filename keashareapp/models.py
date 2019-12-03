27110a9dc2da465d8db7c2c021e27202740ddb97
peln <pern9378@stud.kea.dk>
2019-12-03 16:40:39 +0100

    create group and show groups works

--- keashareapp/models.py
+++ keashareapp/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class AppGroup(models.Model):
-    pass
+    # Meta
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owners')
+    users = models.ManyToManyField(User) # members
+    # Data
+    name = models.CharField(max_length=200)
+    description = models.TextField()
 
+    def __str__(self):
+        return self.name
 
+class Post(models.Model):
+    # Meta
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    group = models.ForeignKey(AppGroup, on_delete=models.CASCADE)
+    # Data
+    last_updated = models.DateTimeField(auto_now=True)
+    text = models.TextField()
 
-
-
-
-
+    def __str__(self):
+        return self.text[:20] # show 20 characters
--- keashareapp/views.py
+++ keashareapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.http import HttpResponseRedirect, HttpResponseBadRequest
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from . import models
 
 @login_required
 def index(request):
@@ -8,12 +10,38 @@ def index(request):
 
 @login_required
 def groups(request):
+    user = User.objects.filter(pk=1)[0]
+
+    # get list of groups user is/is not member
+    if request.method == 'GET':
+        user_groups = models.AppGroup.objects.filter(users__username__contains=user.username)
+        not_user_groups = models.AppGroup.objects.exclude(users__username__contains=user.username)
+
+        context = {
+            'user': user, # the user who is logged in
+            'user_groups': user_groups,
+            'not_user_groups': not_user_groups,
+        }
+
+        return render(request, 'keashareapp/groups.html', context=context)
+
+    # create group
+    if request.method == 'POST':
+        group = models.AppGroup()
+        group.name = request.POST['name']
+        group.description = request.POST['description']
+        group.owner = user
+        group.save() # it needs to be saved first to have a primary key
+        group.users.add(user)
+        group.save() # then save it again with the user
+        return HttpResponseRedirect(reverse('keashareapp:groups'))
+
+    return HttpResponseBadRequest() # if it is neither post or get
 
 
-
-    return render(request, 'keashareapp/groups.html')
-
-
+@login_required
+def posts(request):
+    pass
 
 @login_required
 def groupdetails(request):
@@ -23,9 +51,9 @@ def groupdetails(request):
 def join_group(request):
     pass
 
+
 @login_required
 def leave_group(request):
     pass
-
 
 
